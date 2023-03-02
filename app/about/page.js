b0537601624bcd15396d744b49b1e75a40dda103
Mohamed Cali <mohamedcali@Mohameds-Air.cust.communityfibre.co.uk>
2023-03-02 07:43:32 +0000

    2nd

--- app/about/page.js
+++ app/about/page.js
@@ -6,8 +6,7 @@ const page = () => {
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 justify-center'>
                 <div data-aos='fade-up' data-aos-duration='1500'>
                     <h1 className='text-[40px] lg:text-[40px] lg:max-w-fit font-bold leading-10 lg:leading-[50px] mb-5'>About Us</h1>
-                    <p className='text-lg text-theme-body mb-5 lg:mr-[100px]'>Hair Box is one of the best premium Men salon. Here we provide the best ever services. We believe in 100% customer satisfaction. Hairbox’s services run the gamut from- Hair Cut, Hair Color, Hair Treatment, Facial, Manicure, Pedicure, Body Scrub, Body Massage, and Steam Bath</p>
-                    <p className='text-lg text-theme-body mb-5 lg:mr-[100px]'>Hairbox is more than just your regular makeover salon. All time we ensure that we take on board reliable beauty professionals so that our clients get best services. Hairbox team tries to make sure that you look your best anywhere, anytime, always</p>
+                    <p className='text-lg text-theme-body mb-5 lg:mr-[100px]'>Welcome to Berni's Cutz, the premier hair salon in town! Our salon was founded with a simple mission: to provide our clients with the highest quality haircuts, styling, and coloring services in a warm and welcoming environment. We believe that a great hairstyle has the power to boost confidence, improve self-esteem, and make people feel their very best. That's why we go above and beyond to ensure that each and every client who walks through our doors receives the personalized attention and care they deserve. Whether you're looking for a simple trim, a bold new look, or anything in between, we've got you covered.</p>
                 </div>
                 <div className='flex justify-center' data-aos='fade-up' data-aos-duration='1500'>
                     <img src='/images/image4.jpeg' alt="" className='h-[400px] w-auto self-center object-fil' />
@@ -21,28 +20,11 @@ const page = () => {
           </div>
           <article className="space-y-4">
             <p>
-              Welcome to Curly Hair Salon, where we specialize in helping our clients look and feel their best. 
-            Our team of skilled stylists are dedicated to providing top-quality services in a warm and welcoming atmosphere.
-            </p>
-            <p>
-              We offer a wide range of services, including haircuts, styling, coloring, and more. 
-            Whether you want to update your look with a new haircut or change up your hair color, 
-            we have the expertise and products to help you achieve your desired look.
-            </p>
-            <p>
-              In addition to our styling services, we also offer a variety of hair care products to 
-            help you maintain your new look at home. From shampoo and conditioner to styling products, 
-            we have everything you need to keep your hair looking its best.
-            </p>
-            <p>
-              Thank you for considering Curly Hair Salon for all of your hair care needs. We look forward to 
-            working with you and helping you achieve your desired look. Please do not hesitate to contact 
-            us with any questions or to schedule an appointment.
+              At Berni's Cutz, we're proud to employ a team of highly trained and experienced hair stylists who are passionate about their craft. Our stylists stay up-to-date with the latest trends and techniques in hair cutting, coloring, and styling to ensure that our clients always leave our salon looking and feeling their very best. <br /><br /> We use only the best quality hair care products to ensure that your hair stays healthy and beautiful between visits. We also offer a range of additional services, including blowouts, hair extensions, and updos for special occasions. Come visit us today and experience the Berni's Cutz difference!
             </p>
           </article>
           </div>
         </div>
-
         </div>
     );
 };
