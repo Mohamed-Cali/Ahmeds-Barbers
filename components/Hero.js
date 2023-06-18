6c270128c7f9f6fc938da61f2b1003d7168458c9
Mohamed Cali <mohamedcali@Mohameds-Air.cust.communityfibre.co.uk>
2023-06-18 02:08:49 +0100

    commit

--- components/Hero.js
+++ components/Hero.js
@@ -1,28 +1,34 @@
 import './Hero.css'
 
 function Hero() {
-  const phoneNumber = '07565400486';
+    const phoneNumber = '07872 178484';
 
   return (
     <div>
-        <section className="section3" id="home">
-            <div className="container5 grid">
-            <div className="image1">
-                <img
-                className=''
-                src="/images/image1.jpeg"
-                alt=""
-                />
-            </div>
-            <div className="text">
-                <h2 className="title">Your Go-To Specialist for Modern Haircuts and Beard Grooming</h2>
-                <p>
-                No matter your hair type or preferred style, we have the expertise, grooming products, and contemporary facilities to meet your grooming needs and bring your vision to life.
-                </p>
-                <a className="button1" href={`tel:${phoneNumber}`}>CALL US</a>
-            </div>
-            </div>
-        </section>  
+         <section className="intro">
+          <div className="intro__top"></div>
+          <div className="intro__panel">
+              <div className="intro__picture">
+
+                  <div className="sepia--title">
+                      <img className="sepia__img" src="/images/image1.WebP" alt='hero'/>
+                  </div>
+              </div>
+
+              
+              <h1 className="intro__title">NATS <br/> BARBERS</h1>
+
+              <span className="intro__subtitle">Styling, Barbering, <br />and Personal Care</span>
+
+              <div className='flex flex-row h-full mt-16 sm:flex-col items-center justify-center space-y-5'>
+            <a href={`tel:${phoneNumber}`} className='button1 self-start sm:self-center lg:mt-30 bg-none w-36 p-2 rounded-md animate-bounce duration-300'>
+              <div className='flex flex-col text-xl w-full'>
+                <span className='pr-2 pl-2'>CALL US</span>
+              </div>
+            </a>
+          </div>
+          </div>
+      </section>
     </div>
   )
 }
--- components/Price.js
+++ components/Price.js
@@ -6,69 +6,85 @@ const PricePage = () => {
     <div id='pricing' className="price-page pt-28 mb-20">
       <h1 className="price-page__title mb-10 fs-1 uppercase">Pricing</h1>
       <div className="price-page__services">
-      <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
+        <div className="price-page__service pt-12">
+          <div className="price-page__service-info">
+              <img src='/images/image1.jpeg'/>
+            <h2 className="price-page__service-title">Men's Haircut</h2>
+            <hr className="price-page__service-divider" />
+            <p className="price-page__service-price">£20</p>
+          </div>
         </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
+        <div className="price-page__service pt-12">
+          <div className="price-page__service-info">
+              <img src='/images/image21.jpeg'/>
+            <h2 className="price-page__service-title">Men's Haircut</h2>
+            <hr className="price-page__service-divider" />
+            <p className="price-page__service-price">£20</p>
+          </div>
         </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
+        <div className="price-page__service pt-12">
+          <div className="price-page__service-info">
+              <img src='/images/image1.jpeg'/>
+            <h2 className="price-page__service-title">Men's Haircut</h2>
+            <hr className="price-page__service-divider" />
+            <p className="price-page__service-price">£20</p>
+          </div>
         </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
+        <div className="price-page__service pt-12">
+          <div className="price-page__service-info">
+              <img src='/images/image21.jpeg'/>
+            <h2 className="price-page__service-title">Men's Haircut</h2>
+            <hr className="price-page__service-divider" />
+            <p className="price-page__service-price">£20</p>
+          </div>
         </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
+        <div className="price-page__service pt-12">
+          <div className="price-page__service-info">
+              <img src='/images/image1.jpeg'/>
+            <h2 className="price-page__service-title">Men's Haircut</h2>
+            <hr className="price-page__service-divider" />
+            <p className="price-page__service-price">£20</p>
+          </div>
         </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
+        <div className="price-page__service pt-12">
+          <div className="price-page__service-info">
+              <img src='/images/image21.jpeg'/>
+            <h2 className="price-page__service-title">Men's Haircut</h2>
+            <hr className="price-page__service-divider" />
+            <p className="price-page__service-price">£20</p>
+          </div>
         </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
+        <div className="price-page__service pt-12">
+          <div className="price-page__service-info">
+              <img src='/images/image1.jpeg'/>
+            <h2 className="price-page__service-title">Men's Haircut</h2>
+            <hr className="price-page__service-divider" />
+            <p className="price-page__service-price">£20</p>
+          </div>
         </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
+        <div className="price-page__service pt-12">
+          <div className="price-page__service-info">
+              <img src='/images/image21.jpeg'/>
+            <h2 className="price-page__service-title">Men's Haircut</h2>
+            <hr className="price-page__service-divider" />
+            <p className="price-page__service-price">£20</p>
+          </div>
         </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
+        <div className="price-page__service pt-12">
+          <div className="price-page__service-info">
+              <img src='/images/image1.jpeg'/>
+            <h2 className="price-page__service-title">Men's Haircut</h2>
+            <hr className="price-page__service-divider" />
+            <p className="price-page__service-price">£20</p>
+          </div>
         </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
-        </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
-        </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
-        </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
-        </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
-        </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
-        </div>
-        <div className="price-page__service">
-          <h2 className="price-page__service-title">Men's Haircut</h2>
-          <p className="price-page__service-price">£20</p>
+        <div className="price-page__service pt-12">
+          <div className="price-page__service-info">
+              <img src='/images/image21.jpeg'/>
+            <h2 className="price-page__service-title">Men's Haircut</h2>
+            <hr className="price-page__service-divider" />
+            <p className="price-page__service-price">£20</p>
+          </div>
         </div>
       </div>
         
